c6308e12ff0ea715695195d575623d2bfaea2b41
Rylan Muller <rymuller@il-i3.21-school.ru>
2019-06-28 16:08:35 +0300

    fixed color last elem in stack push.c

--- push.c
+++ push.c
@@ -6,7 +6,7 @@
 /*   By: rymuller <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/01 16:31:13 by rymuller          #+#    #+#             */
-/*   Updated: 2019/06/28 14:55:07 by rymuller         ###   ########.fr       */
+/*   Updated: 2019/06/28 16:05:30 by rymuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,22 @@ char	is_push_command(t_stack *stack, char *line)
 
 void	pb(t_stack *stack)
 {
+	char	flag;
+	size_t	len_a;
+
+	flag = 1;
+	len_a = stack->len_a;
 	if (stack->len_a > 0)
 	{
 		stack->top_b = stack->top_a;
 		stack->top_a = stack->top_a->next;
 		stack->len_a--;
 	}
+	if (!len_a && !stack->len_a)
+		flag = 0;
 	if (stack->print)
 	{
-		if (stack->color && (stack->len_a > 0))
+		if (stack->color && flag)
 			stack->top_b->color = 1;
 		if (stack->color)
 			ft_printf(C">>>>>> pb\n"C);
@@ -49,15 +56,22 @@ void	pb(t_stack *stack)
 
 void	pa(t_stack *stack)
 {
+	char	flag;
+	size_t	len_b;
+
+	flag = 1;
+	len_b = stack->size - stack->len_a;
 	if (stack->size - stack->len_a > 0)
 	{
 		stack->top_a = stack->top_b;
 		stack->top_b = stack->top_b->prev;
 		stack->len_a++;
 	}
+	if (!len_b && !(stack->size - stack->len_a))
+		flag = 0;
 	if (stack->print)
 	{
-		if (stack->color && (stack->size - stack->len_a > 0))
+		if (stack->color && flag)
 			stack->top_a->color = 1;
 		if (stack->color)
 			ft_printf(C">>>>>> pa\n"C);
